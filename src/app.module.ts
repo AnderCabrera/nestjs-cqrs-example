9b53579721ada7c4bfd5df936e684b0b17cbdef3
Ander Cabrera <ander.cabrera@multigroup.com>
2024-11-12 14:56:56 -0600

    adding task.module and adding imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskController } from './modules/task/task.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './modules/task/entities/task.entity';
+import { TaskModule } from './modules/task/task.module';
 
 @Module({
-    imports: [],
-    controllers: [AppController, TaskController],
+    imports: [TypeOrmModule.forRoot({
+        type: "sqlite",
+        database: "database.sqlite",
+        synchronize: true,
+        logging: true,
+        entities: [Task]
+    }), TaskModule],
+    controllers: [AppController],
     providers: [AppService],
 })
 export class AppModule {}
---
+++ src/modules/task/task.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './entities/task.entity';
+import { CqrsModule } from '@nestjs/cqrs';
+import { TaskController } from './task.controller';
+import { CreateTaskHandler } from './cqrs/handlers/create-task.handler';
+import { DeleteTaskHandler } from './cqrs/handlers/delete-task.handler';
+import { UpdateByCompletedHandler } from './cqrs/handlers/update-by-completed.handler';
+import { ListTaskHandler } from './cqrs/handlers/list-task.handler';
+import { ListByIdTaskHandler } from './cqrs/handlers/list-by-id-task.handler';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Task]), CqrsModule],
+    controllers: [TaskController],
+    providers: [
+        CreateTaskHandler,
+        UpdateByCompletedHandler,
+        DeleteTaskHandler,
+        ListTaskHandler,
+        ListByIdTaskHandler
+    ]
+})
+
+export class TaskModule {}
